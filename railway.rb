c8ab937066f3e4e0016efd027c51e378b82c865f
Lesiantus <sashkabonan@gmail.com>
2023-01-29 13:42:03 +0700

    добавлены отступы, удалены сокращения в названиях методов

--- railway.rb
+++ railway.rb
@@ -1,20 +1,25 @@
 class Station
   attr_reader :trains, :name
+
   def initialize(name)
     @name = name
-    @train_list=[]
+    @trains=[]
   end
+
   def arrival(train)
-    @train_list<<train
+    @trains<<train
   end
+
   def trains_on_station
-    @train_list.map { |key, value| return key, value }
+    @trains.map { |key, value| return key, value }
   end
+
   def trains_by_type(type)
-    @train_list.map{ |key, value| puts key.number, key.type if key.type == type }
+    @trains.map{ |key, value| puts key.number, key.type if key.type == type }
   end
+
   def train_depart(train)
-    @train_list.delete(train)
+    @trains.delete(train)
     train.station=nil
   end
 end
@@ -22,15 +27,18 @@ end
 class Train
   attr_accessor :speed, :station, :route
   attr_reader :wagon, :type, :number
+
   def initialize(number, type, wagon=0)
     @number = number
     @type = type
     @wagon = wagon
     @speed = 0
   end
+
   def stop
     @speed=0
   end
+
   def wagon_add
     if @speed == 0
       @wagon+=1
@@ -38,6 +46,7 @@ class Train
       puts "Поезд должен стоять"
     end
   end
+
   def wagon_remove
     if @wagon == 0
       puts "Нечего отцеплять"
@@ -60,6 +69,7 @@ class Train
       station.arrival(self)
     end
   end
+
   def next_previous
     if route.nil?
       puts "Нет маршрута"
@@ -71,21 +81,26 @@ class Train
     end
   end
 end
+
 class Route
   attr_accessor :stations, :depart, :arrive
+
   def initialize(depart, arrive)
     @stations = [depart, arrive]
   end
+
   def add_station(station)
     self.stations.insert(rand(1..self.stations.size-1), station)
   end
-  def del_station(station)
+
+  def delete_station(station)
     if [stations.first, stations.last].include?(station)
       puts "Невозможно удалить первую и последнюю станцию маршрута"
     else
       self.stations.delete(station)
     end
   end
+
   def show_stations
     puts stations.map{|el| el.name}
   end
